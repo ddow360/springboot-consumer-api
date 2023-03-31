0755359be320f7e956c57b5dd6b561fabb0ff500
Drew <andrewbdow@gmail.com>
2023-03-31 17:34:45 -0500

    small updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # springboot-consumer-api
-A base springboot consumer api application to get you started consuming Kafka locally
+A base SpringBoot Consumer API  application to get you started consuming Kafka locally. Benefits of using spring is it takes a lot of the 
+boilerplate code away from the process.
+
+Please create a topic before consuming and populate it with data. Have fun!
--- src/main/java/com/dow/design/springboot/consumer/api/config/KafkaConfig.java
+++ src/main/java/com/dow/design/springboot/consumer/api/config/KafkaConfig.java
@@ -1,6 +1,5 @@
 package com.dow.design.springboot.consumer.api.config;
 
-import com.dow.design.springboot.consumer.api.error.DeserializationErrorHandler;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +10,7 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Configuration
 @EnableKafka
@@ -27,7 +27,7 @@ public class KafkaConfig {
         });
 
         factory.setConsumerFactory(consumerFactory());
-        factory.setCommonErrorHandler(new DeserializationErrorHandler()); // Set a custom error handler
+
         return factory;
     }
 
@@ -37,7 +37,9 @@ public class KafkaConfig {
         // Play with the different configurations such as deserialization, latency, poll and processing
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put( ConsumerConfig.GROUP_ID_CONFIG, "test-group");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "valid_group_id-".concat(UUID.randomUUID().toString()));
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
